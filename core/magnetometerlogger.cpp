15c290b6e0d46a235ade9feb7c9152c17ca4cd89
Zac Berkowitz <zberkowitz@whoi.edu>
2024-07-30 15:13:29 -0400

    add header to magnetometer files

--- core/magnetometerlogger.cpp
+++ core/magnetometerlogger.cpp
@@ -11,6 +11,8 @@ MagnetometerLogger::MagnetometerLogger(Magnetometer *magnetometer, QObject *pare
     // emit on new data
     const auto name = magnetometer->name();
     const auto kind = magnetometer->type();
+
+    sink->setHeader(QStringLiteral("# name: %1\n# kind: %2\n# description: %3\n# Time  x(nT) y(nT) z(nT) total(nT) temperature(degC)\n").arg(name, toString(kind), magnetometer->description())); 
     QObject::connect(magnetometer, &Magnetometer::valueChanged, [=](const auto& value) {
         this->valueChanged(name, kind, QVariant::fromValue(value));
     });
